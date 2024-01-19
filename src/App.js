3462ce8b06fcf1880c541da3d522108c2b5cf3fc
Erik <aortiz.erik@gmail.com>
2024-01-18 23:01:15 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from 'react';
+import Modeler from 'bpmn-js/lib/Modeler';
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import diagramXML from '!!raw-loader!./diagram.bpmn';
+import customModdleExtension from './moddle/custom.json';
 
-function App() {
+import PropertiesView from './properties-panel/PropertiesView'
+
+import "bpmn-js/dist/assets/diagram-js.css";
+import "bpmn-js/dist/assets/bpmn-js.css";
+import "bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css";
+import "./index.css"
+
+function App() {  
+  const container = useRef(null);
+  const [ modeler, setModeler ] = useState(null)
+
+  let modelerInstance = null
+
+  useEffect(() => {
+    if (modelerInstance) return
+
+    modelerInstance = new Modeler({
+      container: container.current,
+      moddleExtensions: {
+        custom: customModdleExtension
+      },
+      keyboard: {
+        bindTo: document.body
+      }
+    });
+    modelerInstance
+      .importXML(diagramXML)
+      .then(({ warnings }) => {
+        if (warnings.length) {
+          console.warn(warnings);
+        }
+  
+        modelerInstance
+          .get("canvas")
+          .zoom("fit-viewport");
+      });  
+
+    setModeler(modelerInstance)
+
+    return () => {
+      modeler?.destroy();
+    }
+  }, [])
+
+  const handleExport = () => {
+    modeler.saveXML({ format: true }).then(({ xml, error }) => {
+      if (error) {
+        console.error(error);
+        return;
+      }
+
+      console.log('UPDATE XML:', xml)
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="modeler-parent">
+      <div id="modeler-container" ref={container}></div>
+      <div id="properties-container">
+        <button onClick={handleExport}>Export BPMN</button>
+        {modeler && 
+          <PropertiesView modeler={ modeler } />
+        }
+      </div>
     </div>
   );
 }
